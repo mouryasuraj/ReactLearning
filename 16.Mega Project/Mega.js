bbeb0b85ae1776a867145b7fc05a8486f1df291d
Suraj <mouryasuraj6266444@gmail.com>
2023-10-30 12:21:11 +0530

    Created Mege Blog Project

---
+++ 16.Mega Project/Mega.js
@@ -0,0 +1,22 @@
+// Process
+
+/*
+
+1.First create project using vite + react
+2.Dependencies:
+    a.@reduxjs/toolkit
+    b.react-redux
+    c.react-router-dom
+    d.appwrite
+    e.@tinymce/tinymce-react
+    f.html-react-parser
+    g.react-hook-form
+
+
+
+3. Environment setUp 
+    a.create .env file in root directory and add this file to .gitIgnore file
+    b.create another .env.sample file in root directory for personal use
+    c.NOTE: If you are using create-react-app then used REACT_APP_ to create variable and if you using other tools then create variable according to the tool. Eg for Vite + react ===> VITE_
+
+*/
